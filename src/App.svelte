4777b584fa8015435503629dc761dd60efa83dd7
TruJared <jared@jaredmakes.com>
2020-09-13 11:41:12 -0400

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,92 @@
+<script>
+    import Keys from './Keys.svelte';
+    import { displayValue } from './stores';
+
+    var eval2 = eval;
+    // https://rollupjs.org/guide/en/#avoiding-eval
+</script>
+
+<style>
+    h1 {
+        margin-top: 0;
+    }
+
+    main {
+        display: flex;
+        align-content: flex-start;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+    .main-img {
+        position: relative;
+    }
+    img {
+        width: 320px;
+    }
+
+    .reset {
+        position: absolute;
+        padding: 5px 10px;
+        top: 42px;
+        z-index: 999;
+        background: transparent;
+        border: none;
+        color: transparent;
+        font-size: 30px;
+        text-transform: uppercase;
+    }
+
+    .reset:active {
+        background: black;
+        color: red;
+    }
+
+    .robot-chest {
+        position: absolute;
+        height: 125px;
+        width: 135px;
+        font-size: 95px;
+        top: 108px;
+        left: 92px;
+        border-radius: 15px;
+        padding: 0;
+        margin: 0;
+        border: none;
+        transform: rotate(180deg);
+        color: #1ce;
+        z-index: 999;
+        background: transparent;
+    }
+
+    .display {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        font-size: 32px;
+        letter-spacing: 0.05em;
+        border: 1px solid #cdcdcd;
+        border-radius: 5px;
+        height: 45px;
+        padding-left: 10px;
+        width: 80%;
+        overflow: scroll;
+    }
+</style>
+
+<main>
+    <!-- <h1>Math Robot That Actually Does Math</h1> -->
+    <button
+        class="button reset"
+        type="reset"
+        on:click={() => ($displayValue = '')}>Reset</button>
+    <div class="main-img">
+        <img src="./robot.png" alt="robot" />
+        <!-- svelte-ignore missing-declaration -->
+        <button
+            class="robot-chest"
+            type="button"
+            on:click={() => ($displayValue = parseInt(eval2($displayValue)).toLocaleString())}>=</button>
+    </div>
+    <div class="display">{$displayValue}</div>
+    <Keys />
+</main>
---
+++ src/Keys.svelte
@@ -0,0 +1,34 @@
+<script>
+	import {displayValue} from './stores'
+
+    const keys = [...Array.from(Array(10).keys()), '+', '-'];
+</script>
+
+<style>
+.keys-wrapper {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: flex-start;
+
+
+}
+
+button {
+    margin: 5px 10px;
+    padding: 15px 25px;
+    background: #ff3e00;
+    color: white;
+}
+
+button:active {
+    background: salmon;
+}
+</style>
+
+<div class='keys-wrapper'>
+{#each keys as key}
+    <button class="button" value="{key}" on:click={()=> $displayValue = `${$displayValue}${key}`}>{key}</button>
+{/each}
+</div>
